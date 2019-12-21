cfd1a9d8c43ee4e73137787562a960870febadb7
Wout Van Damme <wote14@gmail.com>
2019-12-21 19:54:16 +0100

    lots of functionaliy

--- script.js
+++ script.js
@@ -142,13 +142,17 @@ function updateClock(){
         var now2 = new Date();
         // Find the distance between now and the count down date
         var distance = countDownDate.getTime() - now;
-          
+        
         // Time calculations for days, hours, minutes and seconds
       
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
           
+        if (seconds  % 30 === 0 ){
+            checkForStatus();
+            console.log("Checking for status")
+        }
         // Output the result in an element with id="demo"
         document.getElementById("timerStart").innerHTML = hours + "h "
         + minutes + "m " + seconds + "s ";
